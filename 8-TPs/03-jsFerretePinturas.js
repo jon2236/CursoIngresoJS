5f6d79be35d11bc3d3ebb059237fb9394ce7a712
JoNaX KaY <jonatan.quiroga.utn@gmail.com>
2023-01-31 21:52:42 -0300

    bugFixes

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,27 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+        //Fahrenheit a Celsius, simplemente resta 32 y multiplica por 0.5556 (o 5/9).
+        let fah;
+        let cent;
+        
+        fah = parseFloat(document.getElementById("txtIdTemperatura").value);
+        
+        cent = (fah -32) * 0.5556;
+        
+        alert(fah + " grados fahrenheit son " + cent+ " grados centígrados");
+        
+        
+      }
+      
+      function CentigradosFahrenheit () 
+      {
+       //Celsius a Fahrenheit, simplemente multiplica por 1.8 (o 9/5) y suma 32.
+       let fah;
+       let cent;
+       cent = parseInt(document.getElementById("txtIdTemperatura").value);
+       fah = cent * 1.8 + 32
+       alert(cent + " grados centígrados son " + fah + " grados Fahrenheit")
+      
 }
 
-function CentigradosFahrenheit () 
-{
-	
-}
